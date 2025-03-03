75189195f30469efcdfcd2ab5c6de795c1b8b63d
Jaisel Singh <singhjaisel01@gmail.com>
2025-03-03 15:12:40 -0500

    updating policy per episode

--- basic_policy.jl
+++ basic_policy.jl
@@ -2,19 +2,19 @@ using MuJoCo
 using Flux 
 using LinearAlgebra 
 
-model = load_model("carpole.xml")
+model = load_model("cartpole.xml")
 data = init_data(model)
 num_observations = model.qpos + model.qvel # number of observable states 
 num_actions = model.nu # number of actuators 
 
-base_policy = mj_zeros(num_observations, num_actions)
+base_policy = zeros(num_observations, num_actions)
 
 
 num_trajectories = 10 
 num_episodes = 100 # total training episodes 
 max_steps = 1000 # maximum steps per trajectory 
 noise_scale = 0.05 # for policy updates
-
+learning_rate = 0.01
 
 # outermost is training loop -> policy parameters change per steps
 # outer for loop -> sampling policies 
@@ -26,7 +26,6 @@ noise_scale = 0.05 # for policy updates
 for episode in 1:num_episodes
     policies = []
     rewards = [] 
-    trajectories = []
 
     for i in 1:num_trajectories
         # get one or more trajectories and their reward -> add noise every time to the policy 
@@ -51,15 +50,25 @@ for episode in 1:num_episodes
             pos_penalty = 0.1 * abs(data.qpos[1]) #keep the cart centered
             reward = angle_reward - pos_penalty
             total_reward += reward 
-
-            push!(trajectories, data)
         end 
     end 
     
     push!(rewards, total_reward)
     #combine -> weight all the policies with the rewards generated to approximate a gradient direction and take a step 
     # training loop 
+
+    gradient = zeros(size(base_policy))
+    for i in 1:num_trajectories
+    noise = policies[i] - base_policy 
+    gradient += noise*rewards[i]
+    end 
+    base_policy += learning_rate * gradient/num_trajectories 
+
 end 
+
+
+
+
 
 
 
