33dc38f27d0ee9062c4d7e102d042d926d77eea4
Jaisel Singh <singhjaisel01@gmail.com>
2025-03-04 09:59:05 -0500

    changed training

--- basic_policy.jl
+++ basic_policy.jl
@@ -8,7 +8,8 @@ num_observations = model.qpos + model.qvel # number of observable states
 num_actions = model.nu # number of actuators 
 
 base_policy = zeros(num_observations, num_actions)
-
+best_reward = -Inf
+best_policy = base_policy
 
 num_trajectories = 10 
 num_episodes = 100 # total training episodes 
@@ -50,12 +51,20 @@ for episode in 1:num_episodes
             pos_penalty = 0.1 * abs(data.qpos[1]) #keep the cart centered
             reward = angle_reward - pos_penalty
             total_reward += reward 
+
+            if reward >= best_reward
+                best_reward = reward
+                best_policy = copy(policy)
+            end
+
         end 
+        push!(rewards, total_reward)
     end 
     
-    push!(rewards, total_reward)
+
     #combine -> weight all the policies with the rewards generated to approximate a gradient direction and take a step 
     # training loop 
+    rewards = (rewards .- mean(rewards)) / (std(rewards) + 1e-8)
 
     gradient = zeros(size(base_policy))
     for i in 1:num_trajectories
@@ -64,7 +73,21 @@ for episode in 1:num_episodes
     end 
     base_policy += learning_rate * gradient/num_trajectories 
 
+    if episode % 10 == 0
+        println("Episode $episode, Avg Reward: $(mean(rewards)), Best: $best_reward")
+    end
+
 end 
+
+
+function trained_policy_controller!(m::Model, d::Data)
+    state = vcat(d.qpos, d.qvel)
+    d.ctrl .= clamp.(best_policy * state, -1.0, 1.0)
+    nothing
+end
+
+init_visualiser()
+visualise!(model, data, controller = trained_policy_controller!)
 
 
 
