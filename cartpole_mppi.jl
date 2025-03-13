d98aa251e420ab4cfee097b0383a047aa525888e
Jaisel Singh <singhjaisel01@gmail.com>
2025-03-13 10:50:00 -0700

    mppi works sort of decent now

--- cartpole_mppi.jl
+++ cartpole_mppi.jl
@@ -1,6 +1,5 @@
 using MuJoCo 
 
-
 model = load_model("cartpole.xml")
 data = init_data(model)
 
@@ -8,50 +7,55 @@ global const K = 30  #number of trajectories
 global const T = 100 #number of time steps 
 
 # cost function parameters 
-const Σ = 0.1
+const Σ = 1.0
 const ϕ = 0
 const q = 0  
-const λ = 0.1 
+const λ = 1.0
 
 nx = 2 * model.nq # number of states
 nu = model.nu # number of controls 
 
-function terminal_cost(d::data)
-    return 10.0 * running_cost(d)
+function terminal_cost(d, ctrl)
+    return 10.0 * running_cost(d, ctrl)
 end 
 
-function running_cost(d::data)
-    q = zeros(K)
+function running_cost(d, ctrl)
     x = d.qpos[1]
     θ = d.qpos[2]
     x_dot = d.qvel[1]
     θ_dot = d.qvel[2]
-    q =  10*x^2 + (500*(cos(θ)+ 1)^2) + x_dot^2 + 15*(θ_dot^2) #direct cost function that is described in the paper
+
+    pos_cost = 1.0 * x^2
+    theta_cost = 20.0 * (cos(θ) - 1.0)^2
+    vel_cost = 0.1 * x_dot^2
+    thetadot_cost = 0.1 * (θ_dot)^2
+    ctrl_cost = ctrl[1]^2
+    
+    q = pos_cost + theta_cost + vel_cost + thetadot_cost + ctrl_cost
     return q 
 end 
 
 
 global U_t = zeros(nu, T) # global controls 
 
-function mppi(m::data, d::data)
+function mppi(m, d)
     # init_state = vcat(data.qpos, data.qvel)
     ϵ = [randn(nu, T) for k in 1:K]  # sample noise up-front 
+    S = zeros(K)  # costs
 
     for k in 1:K
         d_copy = init_data(m)
         d_copy.qpos .= d.qpos 
         d_copy.qvel .= d.qvel
-
-        S = zeros(K)  # costs
         # weights = zeros(size(ϵ), K)
 
         for t in 1:T 
-            d.ctrl .= U_t[:, t] + ϵ[k][:, t]
-            mj_step!(m, d_copy)
-            S[k] += running_cost(d_copy) + λ * U_t[:, t]' * ϵ[k][:, t] 
+            d_copy.ctrl .= U_t[:, t] + ϵ[k][:, t]
+            step!(m, d_copy)
+            S[k] += running_cost(d_copy, d_copy.ctrl) + λ * U_t[:, t]' * ϵ[k][:, t] 
         end
 
-        S[k] += terminal_cost(d_copy)
+        S[k] += terminal_cost(d_copy, d_copy.ctrl)
     end 
 
         β = minimum(S)
@@ -76,10 +80,10 @@ function mppi(m::data, d::data)
 end 
 
 
-function mppi_controller!(m::model, d::data)
+function mppi_controller!(m, d)
     mppi(m, d)
     d.ctrl .= U_t[:, 1]
 end 
 
-init_visualizer()
+init_visualiser()
 visualise!(model, data, controller = mppi_controller!)
