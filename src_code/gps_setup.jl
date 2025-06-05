4220d973767bcb8a5ea3921c24810a195ce8b7d3
Jaisel Singh <singhjaisel01@gmail.com>
2025-06-05 10:24:21 -0700

    intrduced batching

--- src_code/gps_setup.jl
+++ src_code/gps_setup.jl
@@ -25,26 +25,33 @@ function extract_trajectories(trajectories)
     states = hcat(all_states...)
     actions = hcat(all_actions...)
     return states, actions
-
 end
 
-function train_policy!(policy, trajectories; epochs=1000, lr=0.001)
+function train_policy!(policy, trajectories; epochs=1000, lr=0.001, batch_size=10)
     weights = SimpleChains.init_params(policy, Float32)
     loss(weights, states, actions) = mean(abs2, policy(states, weights) .- actions)
 
     states, actions = extract_trajectories(trajectories)
-    num_samples = size(states, 2) #4 states for 100, time steps hence 100 samples
+    num_samples = size(states, 2) # 4 states for 100, time steps hence 100 samples
 
     opt = Optimisers.Adam(lr)
     opt_state = Optimisers.setup(opt, weights)
 
+
     for epoch in 1:epochs
-        #indices = randperm(num_samples)
-        #states_shuffled = states[:, indices]
-        #actions_shuffled = actions[:, indices]
-        #g = Zygote.gradient(w -> loss(w, states_shuffled, actions_shuffled), weights)
-        g = Zygote.gradient(w -> loss(w, states, actions), weights)
-        opt_state, weights = Optimisers.update(opt_state, weights, g[1])
+        indices = randperm(num_samples)
+        states_shuffled = states[:, indices]
+        actions_shuffled = actions[:, indices] # still 4x1000 size
+
+        # introduce batching
+        for i in 1:batch_size:num_samples
+            end_idx = min(i + batch_size - 1, num_samples)
+
+            states_batch = states_shuffled[:, i:end_idx]
+            actions_batch = actions_shuffled[:, i:end_idx]
+            g = Zygote.gradient(w -> loss(w, states_batch, actions_batch), weights)
+            opt_state, weights = Optimisers.update(opt_state, weights, g[1])
+        end
 
         if epoch % 100 == 0
             current_loss = loss(weights, states, actions)
